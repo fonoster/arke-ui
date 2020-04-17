ea77dc256e8d0521d262acb0b1719bd71c0174fc
Pedro Sanders <sanderspedro@gmail.com>
2020-04-17 19:43:41 -0400

    Improved the Logs ui

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,13 +1,61 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import LogsIcon from '@material-ui/icons/Assignment'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import PauseIcon from '@material-ui/icons/Pause'
+import Button from '@material-ui/core/Button'
 import { observer, inject } from 'mobx-react'
 import { LazyLog, ScrollFollow } from 'react-lazylog'
+import download from 'downloadjs'
 
 class Logging extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sock: null,
+      open: true,
+      reConnect: false
+    }
+  }
 
   render() {
       return (
-        <div style={{ height: 'calc(100vh - 115px)', width: '100%' }}>
+        <div style={{ height: 'calc(100vh - 215px)', width: '100%' }}>
+          <div style={{
+            display: 'flex',
+            alignItems: 'center'
+          }}>
+            <LogsIcon color="primary" size="small" />
+            <Typography color="primary" variant="h6" id="tableTitle">
+                Server's Logs
+            </Typography>
+          </div>
+
+          <div style={{ marginTop: 25, marginBottom: 15 }}>
+            <Button
+            style={{ marginRight: 5 }}
+            variant="contained"
+            size="small"
+            color="secondary"
+            startIcon={this.state.open ? <PauseIcon /> : <PlayArrowIcon />}
+            onClick={() => {
+              if (this.state.open) {
+                this.state.sock && this.state.sock.close()
+              } else {
+                this.setState({reConnect: !this.state.reConnect})
+              }
+              this.setState({open: !this.state.open})
+            }}>Play live</Button>
+
+            <Button
+              onClick={async() =>
+              download(await this.props.apiStore.getSystemLogs(),
+                'routr.log', 'text/plain')}
+              variant="outlined" size="small">Download Logs</Button>
+          </div>
+
           <ScrollFollow
+            key={this.state.reConnect}
             startFollowing={true}
             render={({ follow, onScroll }) => (
               <LazyLog
@@ -18,7 +66,8 @@ class Logging extends React.Component {
                 onScroll={onScroll}
                 websocketOptions={{
                   onOpen: (e, sock) => {
-                    sock.send(JSON.stringify({message: "Socket has been opened!"}))
+                    this.setState({sock})
+                    this.state.sock.send(JSON.stringify({message: "Open channel!"}))
                   },
                   formatMessage: e => e,
                 }}
